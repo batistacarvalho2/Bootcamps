8d58998bfd323f0d546b385f05c6f7d695909f6d
batistacarvalho2 <j.batistacarvalho95@gmail.com>
2021-09-03 16:04:15 -0300

    Commit 1 - App simple de movimentacao bancaria
    Segue implementação de App simples de movimentação bancaria.
    Feita as funções de criação e conta, sacar, depositar e transferir.
    Modulo do BootCamp developer full Stack C#

---
+++ AplicacaoDeTransferenciaBancaria/Dio.Bank/Dio.Bank/Program.cs
@@ -0,0 +1,147 @@
+﻿using Dio.Bank.Enum;
+using System;
+using System.Collections.Generic;
+
+namespace Dio.Bank
+{
+    class Program
+    {
+
+        static List<Conta> listaContas = new List<Conta>();
+        static void Main(string[] args)
+        {
+            // Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "João Batista");
+            // Console.WriteLine(minhaConta.ToString());
+            string opcaoUsuario = ObterOpcaoUsuario();
+            while(opcaoUsuario.ToUpper() != "x")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarContas();
+                        break;
+                    case "2":
+                        InserirContas();
+                        break;                       
+                    case "3":
+                        Tranferir();
+                        break;                       
+                    case "4":
+                        Sacar();
+                        break;                    
+                    case "5":
+                        Depositar();
+                        break;                   
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            Console.WriteLine("Obrigatorio por ultilizar nossos serviços. ");
+
+        }
+
+        private static void Tranferir()
+        {
+            Console.Write("Digite o numero da conta Origem: ");
+            int indiceContaOrigem = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o numero da conta Destino: ");
+            int indiceContaDestino = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser Tranferido: ");
+            double valorTransferencia = double.Parse(Console.ReadLine());
+
+            listaContas[indiceContaOrigem].Transferir(valorTransferencia, listaContas[indiceContaDestino]);
+        }
+
+        private static void Depositar()
+        {
+            Console.Write("Digite o numero da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser Depositado: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+
+            listaContas[indiceConta].Depositar(valorDeposito);
+        }
+
+        private static void Sacar()
+        {
+            Console.Write("Digite o numero da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser sacado: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+
+            listaContas[indiceConta].Sacar(valorSaque);
+        }
+
+        private static void InserirContas()
+        {
+            Console.WriteLine("Inserir nova conta");
+
+            Console.WriteLine("Digite 1 para conta Fisica ou 2 para conta Juridica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do Cliente: ");
+            string entradaNome = Console.ReadLine();
+
+            Console.WriteLine("Digite o saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o Credito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+                                                   saldo: entradaSaldo,
+                                                   credito: entradaCredito,
+                                                   nome: entradaNome);
+
+            listaContas.Add(novaConta);
+        }
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listar contas");
+
+            if(listaContas.Count == 0)
+            {
+                Console.WriteLine("Nenhuma conta cadastrada.");
+                return;
+            }
+
+            for(int i=0; i < listaContas.Count; i++)
+            {
+                Conta conta = listaContas[i];
+                Console.Write("#{0} - ", i);
+                Console.WriteLine(conta);
+
+            }
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Dio Bank a seu dispor!!!");
+            Console.WriteLine("Informe a opção desejada");
+
+            Console.WriteLine("1- Listar a opção desejada");
+            Console.WriteLine("2- Inserir nova conta");
+            Console.WriteLine("3-  Tranferir");
+            Console.WriteLine("4-  Sacar");
+            Console.WriteLine("5- Depositar");
+            Console.WriteLine("6- Limpar Telaaa");
+            Console.WriteLine("7- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
